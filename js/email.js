1f85f8c434caccb128f3d5a42884e01cceed1a74
Esteban Casas <estebancasas9817@gmail.com>
2022-03-30 15:54:24 -0500

    Bug fixed cross section y beam solver email

--- js/email.js
+++ js/email.js
@@ -1,15 +1,16 @@
 // ! ========= SEND EMAIL =========
+'use strict';
 const enviar = document.querySelector('#enviar');
 const nombre = document.querySelector('#nombre');
 const email = document.querySelector('#email');
 const mensaje = document.querySelector('#message');
-const modal = document.querySelector('.modal');
-const modal__error = document.querySelector('.modal__error-mail');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.modal__btn');
+const modal_my_mail = document.querySelector('.modal-mail');
+const modal__my__error = document.querySelector('.modal__error-mail');
+const overlay__mail = document.querySelector('.overlay');
+const btnCloseMyModal = document.querySelector('.btn__email');
 const modal__title_error = document.querySelector('.modal__title-mail');
 const modal__img = document.querySelector('.modal__img');
-const openModal = function (error, flag = 0) {
+const openMyModal = function (error, flag = 0) {
 	if (flag === 0) {
 		modal__img.src = './assets/icons/closeIcon.svg';
 		modal__title_error.textContent = 'Error';
@@ -17,42 +18,42 @@ const openModal = function (error, flag = 0) {
 		modal__img.src = './assets/icons/checkIcon.svg';
 		modal__title_error.textContent = 'Thank you';
 	}
-	modal__error.textContent = error;
-	modal.classList.remove('hidden');
-	overlay.classList.remove('hidden');
+	modal__my__error.textContent = error;
+	modal_my_mail.classList.remove('hidden');
+	overlay__mail.classList.remove('hidden');
 };
-const closeModal = function () {
-	modal.classList.add('hidden');
-	overlay.classList.add('hidden');
+const closeMyModal = function () {
+	modal_my_mail.classList.add('hidden');
+	overlay__mail.classList.add('hidden');
 };
-const closeModalEscape = function () {
-	modal.classList.add('hidden');
-	overlay.classList.add('hidden');
+const closeMyModalEscape = function () {
+	modal_my_mail.classList.add('hidden');
+	overlay__mail.classList.add('hidden');
 };
 
-btnCloseModal.addEventListener('click', closeModal);
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', () => {
-	modal.classList.add('hidden');
-	overlay.classList.add('hidden');
+btnCloseMyModal.addEventListener('click', closeMyModal);
+btnCloseMyModal.addEventListener('click', closeMyModal);
+overlay__mail.addEventListener('click', () => {
+	modal_my_mail.classList.add('hidden');
+	overlay__mail.classList.add('hidden');
 });
 addEventListener('keydown', (e) => {
 	if (e.key === 'Escape') {
-		modal.classList.add('hidden');
-		overlay.classList.add('hidden');
+		modal_my_mail.classList.add('hidden');
+		overlay__mail.classList.add('hidden');
 	}
 });
 enviar.addEventListener('click', (e) => {
 	e.preventDefault();
 
 	if (nombre.value === '' || email.value === '' || mensaje.value === '') {
-		openModal('Input fields are required...', 0);
+		openMyModal('Input fields are required...', 0);
 	} else {
 		if (validateEmail(email.value)) {
 			sendMail(nombre.value, email.value, mensaje.value);
-			openModal('Your email has been succesfully send!', 1);
+			openMyModal('Your email has been succesfully send!', 1);
 		} else {
-			openModal('Incorrect email!', 0);
+			openMyModal('Incorrect email!', 0);
 		}
 
 		nombre.value = '';
@@ -77,24 +78,24 @@ function sendMail(nombre, email, mensaje) {
 	});
 }
 
-const video = document.querySelector('.video');
-const videoPhoto = document.querySelector('.video--photo');
-let onresize = function () {
+const my__video = document.querySelector('.video');
+const my__videoPhoto = document.querySelector('.video--photo');
+let onResize = function () {
 	let width = document.body.clientWidth;
 	if (width <= 900) {
-		video.classList.remove('hidden');
-		videoPhoto.classList.add('hidden');
+		my__video.classList.remove('hidden');
+		my__videoPhoto.classList.add('hidden');
 	} else if (width > 900) {
-		video.classList.add('hidden');
-		videoPhoto.classList.remove('hidden');
+		my__video.classList.add('hidden');
+		my__videoPhoto.classList.remove('hidden');
 	}
 };
-window.addEventListener('resize', onresize);
+window.addEventListener('resize', onResize);
 let width = document.body.clientWidth;
 if (width <= 900) {
-	video.classList.remove('hidden');
+	my__video.classList.remove('hidden');
 	videoPhoto.classList.add('hidden');
 } else if (width > 900) {
-	video.classList.add('hidden');
-	videoPhoto.classList.remove('hidden');
+	my__video.classList.add('hidden');
+	my__videoPhoto.classList.remove('hidden');
 }
